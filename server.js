033312d93e6159169ecfa0607312fd0812a747fb
dave <davedmj1725@gmail.com>
2024-05-18 02:15:51 +0530

    new fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors  = require("cors");
-const mysql = require("mysql")
-//const path = require('path');
+//const mysql = require("mysql")
+const path = require('path');
 const fs = require('fs');
 
 const corsOptions = {
